0237e6149777310dffb318e04dcc3d9136b472a8
jostal <josephtalon01@gmail.com>
2021-11-13 00:03:30 -0500

    added transition

--- jt-portfolio/pages/_app.js
+++ jt-portfolio/pages/_app.js
@@ -1,13 +1,23 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
+import { motion, AnimatePresence } from 'framer-motion'
+import router from 'next/router'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
-    <div>
+    <AnimatePresence exitBeforeEnter>
+      <motion.div
+        key={router.pathname}
+        initial={{x: 2000, opacity: 0, transition: {duration: 1}}}
+        animate={{x: 0, opacity: 1}}
+        exit={{x: -2000, opacity: 0, transition: {duration: 1}}}
+      >
       <Navbar />
       <Component {...pageProps} />
-    </div>
+    </motion.div>
+    </AnimatePresence>
+    
   ) 
   
 }
